941ee73aa2f1f3dd7598801ba3e3db615bb7c056
Ekaterina Garina <kvilcins@mail.ru>
2023-03-16 19:20:03 +0400

    creating dom_2.js

---
+++ script/dom_2.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const headerH1 = document.querySelector('.crm-total__title');
+const tableID = document.querySelector('.head-th_id-width');
+const filterButton = document.querySelector('.interactive__filter');
+const popupAddItem = document.querySelector('.overlay');
+const discountCheckbox = document.querySelector('.inner-fieldset__checkbox');
+const discountInput = document.querySelector('.inner-fieldset__input');
+const popupTotalPrice = document.querySelector('.total__price');
+
