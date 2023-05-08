0d8287eb3b8385847d55be50fcc8d2ed5d2d7278
Ekaterina Garina <kvilcins@mail.ru>
2023-05-09 02:11:48 +0400

    fixing discount script

--- script/inner-fieldset.js
+++ script/inner-fieldset.js
@@ -1,16 +1,26 @@
 'use strict';
 
-discount.onchange = () => {
-  const button = document.body.getElementsByClassName('inner-fieldset__input_active')[0];
-  const colorArray = document.getElementsByClassName('inner-fieldset__input_active');
+const button = document.body.getElementsByClassName('inner-fieldset__input_active')[0];
+const colorArray = document.getElementsByClassName('inner-fieldset__input_active');
+const checkbox = document.querySelector('.inner-fieldset__checkbox');
 
+const cleanInput = () => {
+  colorArray[0].value = "";
+};
+
+checkbox.addEventListener("click", cleanInput);
+
+discount.onchange = () => {
   if (button.disabled) {
     button.disabled = false;
     colorArray[0].style.backgroundColor = '#F4F2FF';
+    colorArray[0].setAttribute('required', '');
   } else {
     button.disabled = true;
     colorArray[0].style.backgroundColor = '#E5E5E5';
-
-    colorArray[0].value = "";
+    colorArray[0].removeAttribute('required', '');
   }
 };
+
+
+
